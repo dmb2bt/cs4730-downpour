259045224b803b00f2401f3653c786c21b95a8ab
Brian McMillen <bkm5kb@virginia.edu>
2014-03-28 00:35:22 -0400

    Part 1 of adding HUD for playtesting

---
+++ Downpour/Downpour/bin/WindowsGL/Debug/HUD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Downpour
+{
+    public class HUD
+    {
+        private Vector2 scorePos = new Vector2(20, 10);
+
+        public SpriteFont Font { get; set; }
+
+        public int Score { get; set; }
+
+        public FontRenderer fontRenderer { get; set; }
+
+        public HUD()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch, FontRenderer fontRenderer)
+        {
+            spriteBatch.Begin();
+  
+            spriteBatch.End();
+        }
+    }
+}
